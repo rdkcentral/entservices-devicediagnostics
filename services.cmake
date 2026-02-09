f88f185f809b81c2176090f59589d7da483a226c
smanes0213 <sankalpmaneshwar46@outlook.com>
2026-02-09 12:04:58 +0530

    Cleanup native build scripts

--- services.cmake
+++ services.cmake
@@ -30,8 +30,6 @@ add_definitions (-DUSE_IARMBUS)
 add_definitions (-DUSE_TR_69)
 add_definitions (-DHAS_API_SYSTEM)
 
-add_definitions(-DRDK_LOG_MILESTONE)
-
 add_definitions (-DUSE_DS)
 
 option(PLUGIN_DEVICEDIAGNOSTICS "PLUGIN_DEVICEDIAGNOSTICS" ON)
