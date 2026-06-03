bd35d796203005c7a6d0982d01db9cf32ee9be46
preeja33 <preeja_raveendran@comcast.com>
2026-06-03 23:03:45 +0530

    Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -54,7 +54,7 @@ git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
 git clone --branch 1.0.6 https://github.com/rdkcentral/entservices-testframework.git
 
 cd ..
-git clone --branch feature/RDKEMW-16743 https://github.com/rdkcentral/entservices-helpers.git
+git clone --branch develop https://github.com/rdkcentral/entservices-helpers.git
 cd "$GITHUB_WORKSPACE"
 ############################
 # Build Thunder-Tools
