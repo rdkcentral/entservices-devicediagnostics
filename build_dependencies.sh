12d83c5619e6b42c444a9bfc3057aa83c1640022
Preeja Raveendran <preeja_raveendran@comcast.com>
2026-06-04 08:35:21 +0530

    build_dependancies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -140,7 +140,7 @@ cmake -G Ninja -S ../entservices-helpers -B build/entservices-helpers \
     -DUSE_THUNDER_R4=ON \
     -DHIDE_NON_EXTERNAL_SYMBOLS=OFF \
     -DPLUGIN_HELPERS=ON \
-    "-DCMAKE_CXX_FLAGS=-I$GITHUB_WORKSPACE/entservices-testframework/Tests/mocks -I$GITHUB_WORKSPACE/entservices-testframework/Tests/headers -include $GITHUB_WORKSPACE/entservices-testframework/Tests/mocks/Iarm.h " \
+    "-DCMAKE_CXX_FLAGS=-I$GITHUB_WORKSPACE/entservices-testframework/Tests/mocks -I$GITHUB_WORKSPACE/entservices-testframework/Tests/headers -I$GITHUB_WORKSPACE/entservices-testframework/Tests/headers/rdk/iarmbus -include $GITHUB_WORKSPACE/entservices-testframework/Tests/mocks/Iarm.h " \
 
 cmake --build build/entservices-helpers --target install
 
