cf02dab9c526d5872bc50ca4c69a47d39c2799aa
RajaLucy <133349260+RajaLucy@users.noreply.github.com>
2026-08-21 21:40:11 +0530

    RDKEMW-22169:Updated version to 4.4.6 (#27)
    * RDKEMW-22169:Updated version to 4.4.6
    
    * RDKEMW-22169:Updated testframework ref and patches to R4.4.6
    
    * RDKEMW-22169: Updating Thunder branch and sha value
    
    * RDKEMW-22169: Fix WorkFlow Failures
    
    * RDKEMW-22169: Fixing L2 WorkFlow Failure
    
    * RDKEMW-22169:Update build_dependencies.sh
    
    * RDKEMW-22169:Update L1-tests.yml
    
    * RDKEMW-22169:Update L2-tests.yml
    
    * RDKEMW-22169:Update L2-tests.yml
    
    ---------
    
    Co-authored-by: srinibas15 <113517102+srinibas15@users.noreply.github.com>
    Co-authored-by: smohap466_comcast <srinibas_mohapatra@comcast.com>

--- cov_build.sh
+++ cov_build.sh
@@ -69,7 +69,7 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-devicediagnostics \
 -Wl,-wrap,system -Wl,-wrap,popen -Wl,-wrap,syslog \
 -DENABLE_TELEMETRY_LOGGING -DUSE_IARMBUS \
 -DENABLE_SET_WAKEUP_SRC_CONFIG -DHAS_API_SYSTEM \
--DHAS_RBUS -DUSE_THUNDER_R4=ON -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
+-DHAS_RBUS  -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
 
 cmake --build build/entservices-devicediagnostics --target install
 echo "======================================================================================"
